f3b056ea0c23f522cd1af6194013ff3840f78b35
Matheus Lopes Santos <informatica.fadenor@unimontes.br>
2018-05-02 16:55:53 -0300

    Adição do DocBlock

--- src/AdorableAvatar.php
+++ src/AdorableAvatar.php
@@ -4,20 +4,45 @@ namespace Igrejanet\Avatar;
 
 use GuzzleHttp\Client;
 
+/**
+ * AdorableAvatar
+ *
+ * @author  Matheus Lopes Santos <fale_com_lopez@hotmail.com>
+ * @version 1.0.0
+ * @since   02/05/2018
+ * @package Igrejanet\Avatar
+ */
 class AdorableAvatar
 {
+    /**
+     * @var Client
+     */
     protected $client;
 
+    /**
+     * @var string
+     */
     protected $address = 'https://api.adorable.io/avatars/285/';
 
+    /**
+     * @var string
+     */
     protected $savePath;
 
-    public function __construct(Client $client, $savePath)
+    /**
+     * @param   Client  $client
+     * @param   string  $savePath
+     */
+    public function __construct(Client $client, string $savePath)
     {
         $this->client   = $client;
         $this->savePath = $savePath;
     }
 
+    /**
+     * @param   mixed  $identifier
+     * @return  bool
+     */
     public function generate($identifier)
     {
         $address    = $this->address . $identifier;
